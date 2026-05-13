00f6cf71f167101f3315e15960a432562cfb54e3
Alejandro de León <pepedoy1213@gmail.com>
2026-05-12 21:03:22 -0600

    Recorridos Fundamentales

--- Base.py
+++ Base.py
@@ -10,3 +10,27 @@ class Nodo:
 class ArbolBinario:
     def __init__(self):
         self.raiz = None
+
+    def preorden(self, nodo, resultado=None):
+        if resultado is None: resultado = []
+        if nodo:
+            resultado.append(nodo.valor)
+            self.preorden(nodo.izquierdo, resultado)
+            self.preorden(nodo.derecho, resultado)
+        return resultado
+
+    def inorden(self, nodo, resultado=None):
+        if resultado is None: resultado = []
+        if nodo:
+            self.inorden(nodo.izquierdo, resultado)
+            resultado.append(nodo.valor)
+            self.inorden(nodo.derecho, resultado)
+        return resultado
+
+    def postorden(self, nodo, resultado=None):
+        if resultado is None: resultado = []
+        if nodo:
+            self.postorden(nodo.izquierdo, resultado)
+            self.postorden(nodo.derecho, resultado)
+            resultado.append(nodo.valor)
+        return resultado
