e79ef54cba19942872fe84b080d2f328bad6c0ba
Alejandro de León <pepedoy1213@gmail.com>
2026-05-12 21:07:34 -0600

    mplementación recursiva de inserción, búsqueda y eliminación por casos

--- Base.py
+++ Base.py
@@ -11,6 +11,7 @@ class ArbolBinario:
     def __init__(self):
         self.raiz = None
 
+    """ Recorridos """
     def preorden(self, nodo, resultado=None):
         if resultado is None: resultado = []
         if nodo:
@@ -34,3 +35,52 @@ class ArbolBinario:
             self.postorden(nodo.derecho, resultado)
             resultado.append(nodo.valor)
         return resultado
+    
+""" Árbol Binario de Búsqueda (BST) """
+class BST(ArbolBinario):
+    def insertar(self, valor):
+        self.raiz = self._insertar_recursivo(self.raiz, valor)
+
+    """ Recursivos """
+    def _insertar_recursivo(self, nodo, valor):
+        if nodo is None:
+            return Nodo(valor)
+        if valor < nodo.valor:
+            nodo.izquierdo = self._insertar_recursivo(nodo.izquierdo, valor)
+        elif valor > nodo.valor:
+            nodo.derecho = self._insertar_recursivo(nodo.derecho, valor)
+        return nodo
+
+    def buscar(self, nodo, valor):
+        if nodo is None or nodo.valor == valor:
+            return nodo
+        if valor < nodo.valor:
+            return self.buscar(nodo.izquierdo, valor)
+        return self.buscar(nodo.derecho, valor)
+
+    def eliminar(self, valor):
+        self.raiz = self._eliminar_recursivo(self.raiz, valor)
+
+    def _eliminar_recursivo(self, nodo, valor):
+        if nodo is None: return nodo
+
+        if valor < nodo.valor:
+            nodo.izquierdo = self._eliminar_recursivo(nodo.izquierdo, valor)
+        elif valor > nodo.valor:
+            nodo.derecho = self._eliminar_recursivo(nodo.derecho, valor)
+        else:
+            # Caso 1 y 2: Nodo con un hijo o sin hijos
+            if nodo.izquierdo is None: return nodo.derecho
+            elif nodo.derecho is None: return nodo.izquierdo
+            
+            # Caso 3: Nodo con dos hijos
+            temp = self._min_valor_nodo(nodo.derecho)
+            nodo.valor = temp.valor
+            nodo.derecho = self._eliminar_recursivo(nodo.derecho, temp.valor)
+        return nodo
+
+    def _min_valor_nodo(self, nodo):
+        actual = nodo
+        while actual.izquierdo is not None:
+            actual = actual.izquierdo
+        return actual
